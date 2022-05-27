463a792d31f927bde692119a2a737bcf5b547fa7
Digionix <jonathanvieri1321@gmail.com>
2022-05-27 16:20:20 +0700

    Added youtube player app

---
+++ youtube-player/app/src/main/java/com/example/youtubeplayerapp/YouTubeConfig.java
@@ -0,0 +1,17 @@
+package com.example.youtubeplayerapp;
+
+public class YouTubeConfig
+{
+    // Instancing Variables
+    private static final String API_KEY = "AIzaSyA_k5diR2BjnCiv9WHhQDMMRELnWcve7Z8";
+
+
+    public YouTubeConfig()
+    {
+
+    }
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
